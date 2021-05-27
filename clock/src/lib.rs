1aa98015df2469f211654c32efe95fb5d9a1eb97
Martin <martin.fontana08@gmail.com>
2021-05-27 21:52:48 +0200

    Clock done

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -8,41 +8,54 @@ pub struct Clock {
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let (hours, minutes) = self.compute();
-        write!(f, "{:0>2}:{:0>2}", hours, minutes)
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock {
+        let mut clock = Clock {
             hours,
             minutes,
-        }
+        };
+        clock.compute();
+
+        clock
     }
 
-    fn compute(&self) -> (i32, i32) {
+    fn compute(&mut self) {
         let mut hours = self.hours;
         if self.hours < 0 {
            hours = 24 + self.hours;
         }
 
-        let mut total_minutes = hours * 60 + self.minutes;
+        let total_minutes = hours * 60 + self.minutes;
 
         hours = total_minutes / 60;
         hours = hours % 24;
 
-        let minutes = total_minutes % 60;
+        let mut minutes = total_minutes % 60;
+        if minutes.is_negative() {
+            let sub_hours = (minutes / 60) - 1;
+            hours += sub_hours;
+            minutes = 60 + minutes;
+        }
 
-        (hours, minutes)
+        if hours.is_negative() {
+            hours = 24 + hours;
+        }
+
+        if hours >= 24 {
+           hours = hours % 24;
+        }
+
+        self.hours = hours;
+        self.minutes = minutes;
     }
 
     pub fn add_minutes(mut self, minutes: i32) -> Self {
         self.minutes += minutes;
-        let (hours, minutes) = self.compute();
-
-        self.hours = hours;
-        self.minutes = minutes;
+        self.compute();
 
         self
     }
